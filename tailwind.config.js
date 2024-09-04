b90a363c5a651fac18f97585ef50ab8456f50b72
Samira <grossisamira@hotmail.com>
2024-09-04 15:36:29 -0300

    feat: adiciona molde de palhetas no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+// (TESTE) criando cores customizadas para a nossa aplicação
+// palhetas de cores preto com roxo?
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            // para chamar ex: bg-primaria ou text-primaria, etc
+            colors: {
+                primaria: '#86198f',
+                secundaria: '',
+                fundo: '#f8fafc',
+                cor_texto: '',
+            },
+        },
     },
     plugins: [],
 }
