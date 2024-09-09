b279809dd403c7fdf4b1babb8137972c4c43f0bf
Samira <grossisamira@hotmail.com>
2024-09-09 13:56:04 -0300

    feat: adicionado Box componente

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -1,5 +1,21 @@
-// fazer o box
-export function Box() {
-    //    terminar de ajustar
-    return <div className="relative box-border max-w[1440] m-auto h-full"></div>
+import { CSSProperties, ReactNode } from 'react'
+
+type BoxProps = {
+    children: ReactNode
+    className?: CSSProperties
+}
+
+const estilos = `
+    relative 
+    box-border 
+    max-w-[1440px] 
+    m-auto h-full 
+    xs-px-8
+    xs-py-4
+    md:px-10 
+    md:py-8
+`
+
+export function Box({ children, className }: BoxProps) {
+    return <div className={`${estilos} ${className}`}>{children}</div>
 }
