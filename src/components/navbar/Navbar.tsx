450c07dc098ab9b8b667f8bdf484385369e4929a
Samira <grossisamira@hotmail.com>
2024-09-09 19:16:25 -0300

    feat: esconder o Footer e o Navbar quando estiver na tela do auth

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import { Alert } from '../alert'
 export function Navbar() {
     const { usuario, handleLogout } = useContext(AuthContext)
     const location = useLocation()
+    const auth = location.pathname === routes.login || location.pathname === routes.cadastro
 
     function logout() {
         handleLogout()
@@ -16,47 +17,50 @@ export function Navbar() {
 
     return (
         <>
-            <div className="w-full bg-primaria text-light flex justify-center py-4">
-                <div className="container flex items-center justify-between text-lg">
-                    <img src="src/assets/logo-bell-hooks.jpg" className="rounded-full max-w-[80px]" />
-                    {/* <Link to={routes.homepage} className="text-2x1 font-bold ml-4">
-                    Centro Bell Hooks
-                </Link> */}
+            {!auth && (
+                <>
+                    <div className="w-full bg-primaria text-light flex justify-center py-4">
+                        <div className="container flex items-center justify-between text-lg">
+                            <Link to={routes.homepage}>
+                                <img src="src/assets/logo-bell-hooks.jpg" className="rounded-full max-w-[80px]" />
+                            </Link>
 
-                    <div className="flex gap-4">
-                        {location.pathname === routes.homepage ? (
-                            <>
-                                <div className="hover:underline font-bold">Sobre</div>
-                                <div className="hover:underline font-bold">Contato</div>
-                            </>
-                        ) : (
-                            <>
-                                <Link to={routes.servicos} className="hover:underline font-semibold">
-                                    Serviços
-                                </Link>
-                                <Link to={routes.cadastrarServico} className="hover:underline font-semibold">
-                                    Cadastrar Serviços
-                                </Link>
-                                <Link to={routes.categorias} className="hover:underline font-semibold">
-                                    Categorias
-                                </Link>
-                                <Link to={routes.cadastrarCategoria} className="hover:underline font-semibold">
-                                    Cadastrar Categoria
-                                </Link>
+                            <div className="flex gap-4">
+                                {location.pathname === routes.homepage ? (
+                                    <>
+                                        <div className="hover:underline font-bold">Sobre</div>
+                                        <div className="hover:underline font-bold">Contato</div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Link to={routes.servicos} className="hover:underline font-semibold">
+                                            Serviços
+                                        </Link>
+                                        <Link to={routes.cadastrarServico} className="hover:underline font-semibold">
+                                            Cadastrar Serviços
+                                        </Link>
+                                        <Link to={routes.categorias} className="hover:underline font-semibold">
+                                            Categorias
+                                        </Link>
+                                        <Link to={routes.cadastrarCategoria} className="hover:underline font-semibold">
+                                            Cadastrar Categoria
+                                        </Link>
 
-                                <Link
-                                    to={usuario.token ? routes.homepage : routes.login}
-                                    className="hover:underline font-bold"
-                                    onClick={usuario.token ? logout : undefined}
-                                >
-                                    {usuario.token === '' ? 'Login' : 'Sair'}
-                                </Link>
-                            </>
-                        )}
+                                        <Link
+                                            to={usuario.token ? routes.homepage : routes.login}
+                                            className="hover:underline font-bold"
+                                            onClick={usuario.token ? logout : undefined}
+                                        >
+                                            {usuario.token === '' ? 'Login' : 'Sair'}
+                                        </Link>
+                                    </>
+                                )}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <hr className="bg-secundaria h-1" />
+                    <hr className="bg-secundaria h-1" />
+                </>
+            )}
         </>
     )
 }
