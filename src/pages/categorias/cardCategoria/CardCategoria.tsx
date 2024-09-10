66921d568905cdf85fc9c47f34ccf540b8f9da2b
Samira <grossisamira@hotmail.com>
2024-09-09 23:11:33 -0300

    fix: Ajusta estilos de CardCategoria e CardServico

--- src/pages/categorias/cardCategoria/CardCategoria.tsx
+++ src/pages/categorias/cardCategoria/CardCategoria.tsx
@@ -12,10 +12,13 @@ export function CardCategoria({ categoria }: CardCategoriasProps) {
         <div className="flex flex-col">
             <Card>
                 <CardContent>
-                    <CardTitle className="text-lg font-semibold mt-4 mb-2">Categoria</CardTitle>
-                    <p className="mb-4">Cargos: {categoria.cargo}</p>
+                    <div className="flex mt-4 mb-2 items-end">
+                        <p>
+                            <strong className="text-lg font-semibold">Categoria: </strong> {categoria.cargo}
+                        </p>
+                    </div>
 
-                    <div className="flex gap-4">
+                    <div className="flex gap-4 mt-4">
                         <Link to={`/editar-categoria/${categoria.id}`} className="w-full ">
                             <Button variant="outline">Editar</Button>
                         </Link>
--- src/pages/servicos/cardServico/CardServico.tsx
+++ src/pages/servicos/cardServico/CardServico.tsx
@@ -12,33 +12,38 @@ export function CardServico({ servico }: CardServicoProps) {
     const data = formatarData(servico.data)
 
     return (
-        <div className="h-screen">
-            <Card className="flex flex-col">
-                <CardTitle title={servico.titulo} />
-                <CardContent>
-                    <div className="flex flex-col py-4">
-                        <h4 className="text-lg font-semibold uppercase">{servico.nome}</h4>
-                        <p className="mt-2">{servico.descricao}</p>
-                        <div className="mt-4">
-                            <p className="text-sm">Categoria: {servico.categoria?.cargo}</p>
-                            <p className="text-sm">Data: {data}</p>
-                        </div>
-                    </div>
+        <Card>
+            <CardContent>
+                <CardTitle className="text-xl my-4 font-semibold">{servico.titulo}</CardTitle>
 
-                    <div className="flex gap-4">
-                        <Link to={`/editar-servico/${servico.id}`} className="w-full">
-                            <Button className="w-full" variant="outline">
-                                Editar
-                            </Button>
-                        </Link>
-                        <Link to={`/deletar-servico/${servico.id}`} className="w-full">
-                            <Button className="w-full" variant="outline">
-                                Deletar
-                            </Button>
-                        </Link>
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
+                <div className="flex flex-col gap-1 mb-4">
+                    <p>
+                        <strong className="font-semibold">Empresa: </strong> {servico.nome}
+                    </p>
+                    <p>
+                        <strong className="font-semibold">Descrição: </strong> {servico.descricao}
+                    </p>
+                    <p>
+                        <strong className="font-semibold">Categoria: </strong> {servico.categoria?.cargo}
+                    </p>
+                    <p>
+                        <strong className="font-semibold">Data: </strong> {data}
+                    </p>
+                </div>
+
+                <div className="flex gap-4">
+                    <Link to={`/editar-servico/${servico.id}`} className="w-full">
+                        <Button className="w-full" variant="outline">
+                            Editar
+                        </Button>
+                    </Link>
+                    <Link to={`/deletar-servico/${servico.id}`} className="w-full">
+                        <Button className="w-full" variant="outline">
+                            Deletar
+                        </Button>
+                    </Link>
+                </div>
+            </CardContent>
+        </Card>
     )
 }
