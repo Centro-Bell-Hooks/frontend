dec56d7d5422bb67e63666fb386537045412ff11
Liara Cristina <lidskey@hotmail.com>
2024-09-27 19:54:59 -0300

    feat: responsividade do texto sobre o carrossel, necessario fix

--- src/pages/homepage/carrossel/Carrossel.tsx
+++ src/pages/homepage/carrossel/Carrossel.tsx
@@ -31,18 +31,20 @@ export function Carrossel() {
                         src="/assets/resized1.jpeg"
                         alt="Carrossel - Slide 01"
                     />
-                    <div className="absolute max-w-[40%] top-1/2 left-[30%] transform -translate-x-1/2 -translate-y-1/2 text-white text-shadow p-4 rounded">
-                        <h2 className="text-3xl font-bold mb-2">Oferecemos cursos gratuitos que transformam vidas.</h2>
-                        <p className="text-xl">Aprenda novas habilidades e conecte-se com oportunidades.</p>
+                    <div className="absolute max-w-[40%] top-1/2 left-[30%] transform -translate-x-1/2 -translate-y-1/2 text-white text-shadow p-4 rounded ">
+                        <h2 className="md:text-3xl font-bold mb-2">
+                            Oferecemos cursos gratuitos que transformam vidas.
+                        </h2>
+                        <p className="md:text-xl">Aprenda novas habilidades e conecte-se com oportunidades.</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="relative">
                     <img className="swiper-slide-img gap-4" src="/assets/resized2.jpeg" alt="Carrossel - Slide 02" />
                     <div className="absolute max-w-[40%] top-1/2 left-[30%] transform -translate-x-1/2 -translate-y-1/2 text-white text-shadow p-4 rounded">
-                        <h2 className="text-3xl font-bold mb-2">
+                        <h2 className="md:text-3xl font-bold mb-2">
                             Gostaria de uma nova oportunidade no mercado de trabalho?
                         </h2>
-                        <p className="text-xl max-w-[80%]">
+                        <p className="md:text-xl max-w-[80%]">
                             Nossos cursos são feitos para apoiar você na sua jornada em garantir um futuro melhor.
                         </p>
                     </div>
@@ -50,8 +52,8 @@ export function Carrossel() {
                 <SwiperSlide className="relative">
                     <img className="swiper-slide-img" src="/assets/resized3.jpeg" alt="Carrossel - Slide 03"></img>
                     <div className="absolute max-w-[40%] top-1/2 left-[30%] transform -translate-x-1/2 -translate-y-1/2 text-white text-shadow p-4 rounded">
-                        <h2 className="text-3xl font-bold mb-2">Juntas, podemos superar desafios. </h2>
-                        <p className="text-xl">
+                        <h2 className="md:text-3xl font-bold mb-2">Juntas, podemos superar desafios. </h2>
+                        <p className="md:text-xl">
                             "No momento em que escolhemos amar, começamos a nos mover contra a dominação, contra a
                             opressão. No momento em que escolhemos amar, começamos a nos mover em direção à liberdade, a
                             agir de formas que libertam a nós e aos outros." - Bell Hooks
