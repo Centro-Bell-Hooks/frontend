07592c10ef3bc2c60c8fdea7f418c6d260fe3554
Samira <grossisamira@hotmail.com>
2024-10-08 20:03:57 -0300

    fix: ajustado o espaçamento do CardServico

--- src/pages/servicos/cardServico/CardServico.tsx
+++ src/pages/servicos/cardServico/CardServico.tsx
@@ -19,7 +19,7 @@ export function CardServico({ servico }: CardServicoProps) {
     }
 
     return (
-        <Card className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl mx-auto p-4">
+        <Card>
             <CardContent>
                 <CardTitle className="text-xl my-4 font-semibold text-center">{servico.titulo}</CardTitle>
 
