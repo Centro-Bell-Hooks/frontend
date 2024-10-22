c2dd52ac54bb6a922b6518c6a305b232ac920d90
Samira <grossisamira@hotmail.com>
2024-10-22 13:04:20 -0300

    fix: ajustado a responsividade de deletar categoria

--- src/pages/categorias/deletarCategoria/DeletarCategoria.tsx
+++ src/pages/categorias/deletarCategoria/DeletarCategoria.tsx
@@ -1,36 +1,36 @@
-import { useState, useContext, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useState, useContext, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
-import { AuthContext } from '../../../contexts';
-import { Categoria } from '../../../models';
-import { buscar, deletar } from '../../../services';
-import { routes } from '../../../routes';
-import { Alert, Button, Card, CardContent, CardTitle, Box } from '../../../components';
+import { AuthContext } from '../../../contexts'
+import { Categoria } from '../../../models'
+import { buscar, deletar } from '../../../services'
+import { routes } from '../../../routes'
+import { Alert, Button, Card, CardContent, CardTitle, Box } from '../../../components'
 
 export function DeletarCategoria() {
-    const navigate = useNavigate();
-    const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
-    const { id } = useParams<{ id: string }>();
-    const { usuario, handleLogout } = useContext(AuthContext);
-    const token = usuario.token;
+    const navigate = useNavigate()
+    const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
+    const { id } = useParams<{ id: string }>()
+    const { usuario, handleLogout } = useContext(AuthContext)
+    const token = usuario.token
 
     useEffect(() => {
-        if (id !== undefined) buscarPorId(id);
+        if (id !== undefined) buscarPorId(id)
         if (token === '') {
-            Alert({ mensagem: 'Você precisa estar logado.', tipo: 'info' });
-            navigate(routes.login);
+            Alert({ mensagem: 'Você precisa estar logado.', tipo: 'info' })
+            navigate(routes.login)
         }
-    }, [token, id]);
+    }, [token, id])
 
     async function buscarPorId(id: string) {
         try {
             await buscar(`/categorias/${id}`, setCategoria, {
                 headers: { Authorization: token },
-            });
+            })
         } catch (error: any) {
             if (error.toString().includes('401')) {
-                Alert({ mensagem: 'O token expirou, favor logar novamente.', tipo: 'info' });
-                handleLogout();
+                Alert({ mensagem: 'O token expirou, favor logar novamente.', tipo: 'info' })
+                handleLogout()
             }
         }
     }
@@ -39,32 +39,42 @@ export function DeletarCategoria() {
         try {
             await deletar(`/categorias/${id}`, {
                 headers: { Authorization: token },
-            });
-            Alert({ mensagem: 'Categoria apagada com sucesso!' });
-            navigate(routes.categorias);
+            })
+            Alert({ mensagem: 'Categoria apagada com sucesso!' })
+            navigate(routes.categorias)
         } catch (error) {
-            Alert({ mensagem: 'Erro ao apagar a Categoria, consulte o administrador.', tipo: 'error' });
+            Alert({ mensagem: 'Erro ao apagar a Categoria, consulte o administrador.', tipo: 'error' })
         }
     }
 
     return (
-        <Box className="min-h-screen p-4 sm:p-8">
-            <Card className="w-full max-w-md shadow-lg mx-auto">
-                <CardContent>
-                    <CardTitle className="text-xl my-4 font-semibold text-center">Você deseja mesmo apagar?</CardTitle>
-                    <p>
-                        <strong className="font-semibold">Cargo:</strong> {categoria.cargo}
-                    </p>
-                    <div className="flex flex-col sm:flex-row gap-4 mt-5">
-                        <Button variant="outline" onClick={() => navigate(routes.categorias)} className="w-full sm:w-auto">
-                            Não
-                        </Button>
-                        <Button onClick={deletarCategoria} className="w-full sm:w-auto">
-                            Sim
-                        </Button>
-                    </div>
-                </CardContent>
-            </Card>
-        </Box>
-    );
+        <div className="max-sm:pt-[130px]">
+            <Box className="p-4 sm:p-8">
+                <div className="flex items-center justify-center h-[90vh] sm:h-screen">
+                    <Card className="w-full max-w-sm shadow-lg m-auto p-6">
+                        <CardContent>
+                            <CardTitle className="text-xl my-4 font-semibold text-center">
+                                Você deseja mesmo apagar?
+                            </CardTitle>
+                            <p>
+                                <strong className="font-semibold">Cargo:</strong> {categoria.cargo}
+                            </p>
+                            <div className="flex gap-4 mt-5">
+                                <Button
+                                    variant="outline"
+                                    onClick={() => navigate(routes.categorias)}
+                                    className="w-full"
+                                >
+                                    Não
+                                </Button>
+                                <Button onClick={deletarCategoria} className="w-full">
+                                    Sim
+                                </Button>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </Box>
+        </div>
+    )
 }
